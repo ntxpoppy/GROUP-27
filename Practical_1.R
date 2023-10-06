039bb1f44076f9d5a83619f39043a0e89755c0d9
anaghaindulal <145323405+anaghaindulal@users.noreply.github.com>
2023-10-06 01:46:31 +0100

    Updated Anagha's Part(II)
    Added question 8, cleaned code. Final. Code is good to go.

--- Practical_1.R
+++ Practical_1.R
@@ -48,7 +48,7 @@ a <- gsub("_(","",a,fixed=TRUE) ## remove "_("
 
 
 # ------- Ans 4,5 ------- #
-#Anagha's part for Practical-1
+#Anagha's part for Practical-1(I)
 
 #Creating split_punct() AND Separating punctuation marks and words
 
@@ -211,6 +211,67 @@ req_matrix_P <- cbind(col_1_P,col_2,deparse.level=0)
 row_sum_P <- rowSums(req_matrix_P)
 na_row_index_P <- which(is.na(row_sum_P))
 P <- req_matrix_P[-na_row_index_P,]
+
+# ------- Ans8 ------- #
+#Anagha's part for Practical-1(II)
+
+##Question 8: To simulate 50-word sections from the model
+
+#Function to generate a 50-word section
+
+#defining the function simu_fn() to generate 50 words
+simu_fn <- function(words, T, P) {
+  
+  #Initializing an empty character vector to store a word
+  char <- character(0)  
+  
+  #Starting with two initial words using sample()
+  first_word <- sample(words, 1)
+  second_word <- sample(words, 1)
+  
+  #Creating the loop for generating 50 words
+  for (i in 1:50) {
+    # Extracting sub-matrix by finding rows in T where first_word is in the first column and second_word is in the second column
+    #using which() function by checking their indices
+    find_rows <- which(T[, 1] == first_word & T[, 2] == second_word)
+    
+    if (length(find_rows) > 0) {
+      # Randomly selecting one of the matching rows
+      select_row <- sample(find_rows, 1)
+      
+      #Getting the next word from the third column of the selected row
+      next_word <- words[T[select_row, 3]]
+    } else {
+      # If no matching rows in T, checking P
+      match_in_p <- which(P[, 1] == first_word)
+      if (length(match_in_p) > 0) {
+        # Randomly selecting a row from P and getting the second word as the next word
+        select_row <- sample(match_in_p, 1)
+        next_word <- words[P[select_row, 2]]
+      } else {
+        # If no matching pairs in P, choose a common word randomly
+        next_word <- sample(words, 1)
+      }
+    }
+    
+    # Adding the next word to the total text as a vector
+    char <- c(char, next_word)
+    
+    # Updating first_word and second_word for the next iteration
+    first_word <- second_word
+    second_word <- next_word
+  }
+  
+  # Returning the generated text as a single string
+  return(paste(char, collapse = " "))
+}
+
+# Defining a 50-word section as para
+para <- simu_fn(b, T, P)
+
+# Printing the generated section with cat
+cat(para, "\n")
+
 
 # ------- Ans 10 ------- #
 #Navya's part for Practical-1
